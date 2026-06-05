1ad65b069c049a31a096e5381e409f3a9bc8518c
JH-Kolya <163450321+JH-Kolya@users.noreply.github.com>
2026-06-05 13:04:30 +0100

    Delete countdown-banner-pro-v5-project/countdown-banner-pro.php

--- countdown-banner-pro-v5-project/countdown-banner-pro.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Plugin Name: Countdown Banner Pro
-Version: 5.0
-*/
-if(!defined('ABSPATH')) exit;
-require_once plugin_dir_path(__FILE__).'includes/class-cbp-plugin.php';
-CBP_Plugin::instance();
